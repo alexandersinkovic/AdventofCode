aa3f4d0ac36c02e319344aacfdc5ad96b5076ca8
Alexander <alexander.sinkovic@gmail.com>
2023-12-02 06:52:40 +0100

    Clear outputs

--- 2020/aoc_fetcher.py
+++ 2020/aoc_fetcher.py
@@ -2,6 +2,6 @@ import requests
 import browser_cookie3
 
 def get_data(year, day):
-    cookies = browser_cookie3.chrome(domain_name='.adventofcode.com')
+    cookies = browser_cookie3.firefox(domain_name='.adventofcode.com')
     response = requests.get(f'https://adventofcode.com/{year}/day/{day}/input', verify=True, cookies=cookies, timeout=3)
     return response.text

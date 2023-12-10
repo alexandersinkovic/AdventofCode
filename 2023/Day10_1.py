e133fc70c098a01450d55de8b595e084d2941cdf
Alexander <alexander.sinkovic@gmail.com>
2023-12-10 16:03:28 +0100

    Day 10

---
+++ 2023/Day10_1.py
@@ -0,0 +1,56 @@
+from aoc_fetcher import get_data
+
+input = get_data(2023, 10).splitlines()
+
+pipemap = {'|': [(-1, 0), (1, 0)], '-': [(0, -1), (0, 1)], 'L': [(-1, 0), (0, 1)], 'J': [(-1, 0), (0, -1)], '7': [(0, -1), (1, 0)], 'F': [(1, 0), (0, 1)]}
+
+starty = 0
+startx = 0
+for i in range(len(input)):
+    for j in range(len(input[0])):
+        if input[i][j] == 'S':
+            starty = i
+            startx = j
+
+check = [[-1 for j in range(len(input[0]))] for i in range(len(input))]
+check[starty][startx] = 0
+
+def isconnected(sy, sx, dy, dx):
+    pipe = input[dy][dx]
+    if pipe == '.':
+        return False
+    connections = pipemap[pipe]
+    for i in range(2):
+        connection = connections[i]
+        if dy + connection[0] == sy and dx + connection[1] == sx:
+            return True
+    return False
+
+def findStartingDirections(sy, sx):
+    res = []
+    for i in range(-1, 2, 2):
+        if isconnected(sy, sx, sy + i, sx):
+            res.append((sy + i, sx))
+        if isconnected(sy, sx, sy, sx + i):
+            res.append((sy, sx + i))
+    return res
+
+directions = findStartingDirections(starty, startx)
+y, x = directions[0]
+goaly, goalx = directions[1]
+check[y][x] = 1
+print(y, starty)
+print(x, startx)
+counter = 1
+while y != goaly or x != goalx:
+    pipe = input[y][x]
+    neighbors = pipemap[pipe]
+    for neighbor in neighbors:
+        if check[y+neighbor[0]][x+neighbor[1]] == -1:
+            check[y+neighbor[0]][x+neighbor[1]] = counter+1
+            counter += 1
+            y = y+neighbor[0]
+            x = x+neighbor[1]
+            break
+print(counter/2)
+#print(input[y][x])

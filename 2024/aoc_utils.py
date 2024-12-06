a57d10431c84a98e2649bc0edbc8b49efa58abcb
Alexander <alexander.sinkovic@gmail.com>
2024-12-06 07:53:01 +0100

    Dict utility

--- 2024/aoc_utils.py
+++ 2024/aoc_utils.py
@@ -3,3 +3,9 @@ DIRS9 = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (
 
 def splitTwice(input):
     return [[c for c in line] for line in input.split('\n')]
+
+def add_to_dict(k, v, dict):
+    if k in dict:
+        dict[k].append(v)
+    else:
+        dict[k] = [v]

729ecf5b5c31550ae7f36eeabd11e520e8c897eb
Alexander <alexander.sinkovic@gmail.com>
2024-12-06 20:02:10 +0100

    Day7 prep

---
+++ 2024/day7.py
@@ -0,0 +1,17 @@
+from aocd import data
+
+data = data.split('\n')
+
+#f = open('day7in.txt', 'r').read()
+#f = open('day7test.txt', 'r').read()
+#input = f.split('\n')
+
+def part1(input):
+    pass
+
+def part2(input):
+    pass
+
+
+part1(data)
+#part2(data)

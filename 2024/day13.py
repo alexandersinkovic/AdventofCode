d16cf649c06b02c911b00ef97721d860dff6480c
Alexander Sinkovic <alexander.sinkovic@f-i-sp.de>
2024-12-13 10:22:57 +0100

    Neuer Ansatz funktioniert auch nicht

--- 2024/day13.py
+++ 2024/day13.py
@@ -3,25 +3,7 @@ from aocd import data
 #data = open('day13test.txt', 'r').read()
 machines = data.split('\n\n')
 
-
-def calcPrize(ax, ay, bx, by, px, py, cx, cy, acount, bcount, minTokens, cache):
-    if cache[acount][bcount] == 1 or cx > px or cy > py or acount > 100 or bcount > 100 or (acount*3 + bcount) > minTokens:
-        return (minTokens, False)
-    cache[acount][bcount] = 1
-    if cx == px and cy == py and (acount*3 + bcount) < minTokens:
-        return (acount*3 + bcount, True)
-    pressATokens, successA = calcPrize(ax, ay, bx, by, px, py, cx+ax, cy+ay, acount+1, bcount, minTokens, cache)
-    pressBTokens, successB = calcPrize(ax, ay, bx, by, px, py, cx+bx, cy+by, acount, bcount+1, minTokens, cache)
-    if successA and successB:
-        if pressATokens < pressBTokens:
-            return (pressATokens, True)
-        else:
-            return (pressBTokens, True)
-    if successA:
-        return (pressATokens, True)
-    return (pressBTokens, True)
-
-
+# a * x + b * y = p
 
 def part1():
     resTokens = 0
@@ -40,16 +22,26 @@ def part1():
         px, py = p.split(', ')
         px = int(px.split('=')[1])
         py = int(py.split('=')[1])
-        #print(ax, ay)
-        #print(bx, by)
-        #print(px, py)
         if ax*100 + bx*100 < px or ay*100 + by*100 < py:
             print("Skipping machine")
             continue
-        tokens, success = calcPrize(ax, ay, bx, by, px, py, 0, 0, 0, 0, 400, cache)
-        print(tokens, success)
-        if success:
-            resTokens += tokens
+        possibleA = [a for a in range(0, 101) if (px - (a*ax)) % bx == 0 and (py - (a*ay)) % by == 0]
+        ways = [[a, (px - (a*ax))/ bx] for a in possibleA]
+        if len(ways) > 1:
+            print(ax, ay)
+            print(bx, by)
+            print(px, py)
+            print(ways)
+            ways = list(filter(lambda x: 0 <= x[1] <= 100, ways))
+            print(ways)
+            if len(ways) >= 1:
+                tokens = sorted(map(lambda x: x[0]*3 + x[1], ways))
+                print('tokens', tokens)
+                resTokens += tokens[0]
+        #tokens, success = calcPrize(ax, ay, bx, by, px, py, 0, 0, 0, 0, 400, cache)
+        #print(tokens, success)
+        #if success:
+        #    resTokens += tokens
 
     print("Result:", resTokens)
 
